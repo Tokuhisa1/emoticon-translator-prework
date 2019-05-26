820fa86fdc1ac40a1daa71f71f2892b9eabd4a43
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-05-26 19:08:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,18 @@ require 'yaml'
 
 def load_library(path)
   # code goes here
-  YAML.load_file(path)
-  # YAML.dump(path)
+  library = {}
+  get = ['get_emoticon', 'get_meaning']
+
+  for i in 0...get.length
+    library[get[i]] = {}
+
+    YAML.load_file(path).each do |key, value|
+      library[get[i]][key] = value[i]
+    end
+  end
+
+  library
 end
 
 def get_japanese_emoticon
