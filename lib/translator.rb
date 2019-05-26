72ae4fb0a2eecba368bd69607a0059931912a77c
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-05-26 20:29:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,18 @@ end
 
 def get_japanese_emoticon
   # code goes here
+  library = {}
+  get = ['get_emoticon', 'get_meaning']
+
+  library[get[0]] = {}
+  library[get[1]] = {}
+
+  YAML.load_file(path).each do |key, value|
+    library[get[0]][value[0]] = value[1]
+    library[get[1]][value[1]] = key
+  end
+
+  library
 end
 
 def get_english_meaning
